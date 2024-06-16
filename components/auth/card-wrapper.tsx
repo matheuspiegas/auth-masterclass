d78410d02042ffb31524d91056057c89fbb0c111
matheuspiegas <matheusaugustopiegassales@gmail.com>
2024-06-16 17:12:49 -0300

    creating card-wrapper component

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardFooter,
+} from "@/components/ui/card";
+import { Header } from "@/components/auth/header";
+import { Social } from "@/components/auth/social";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  backButtonHref,
+  backButtonLabel,
+  headerLabel,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+    </Card>
+  );
+};
