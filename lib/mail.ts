1f1ab58d4b16a923b5426c03c4022d4c592bffe0
Matheus Sales <103012141+matheuspiegas@users.noreply.github.com>
2024-08-05 16:42:35 -0300

    Update mail.ts

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@authmasterclass.guru",
     to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA code: ${token}</p>`,
@@ -16,7 +16,7 @@ export const sendTwoFactorEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@authmasterclass.guru",
     to: email,
     subject: "Confirm your email!",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
@@ -26,7 +26,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@authmasterclass.guru",
     to: email,
     subject: "Confirm your email!",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
